8f24d41bb3027058b1e8718bc01030681c649d27
elisAlphaCode <99659934+elisAlphaCode@users.noreply.github.com>
2022-04-18 17:01:40 +0100

    Update main.h

--- main.h
+++ main.h
@@ -7,7 +7,7 @@ int _putchar(char c);
 int display_string(va_list arg);
 int display_STRING(va_list arg);
 int display_char(va_list arg);
-int display_percent(va_list arg);
+int display_percent(void);
 int _printf(const char *format, ...);
 int display_unsignedIntToHex(unsigned int num, char _case);
 
